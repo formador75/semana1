8af0edb32d8f3d8d4cd5ab68926dda975f1d1e35
formador75 <misiontic.formador75@uis.edu.co>
2022-07-19 05:50:06 -0500

    test

--- retos_java/reto3.java
+++
@@ -1,9 +0,0 @@
-public class ProductoNoRefrigerado extends Producto {
-    
-    
-    public double calcularCostoDeAlmacenamiento(){
-     
-        return this.getValorBase()*1.1;
-        }
-    
-}
